2adc2d4c2f9be00e663fb4a1b3088e828e93076c
Yuvan Sankar Raj <u1sankarraj1@gmail.com>
2026-03-19 12:09:33 +0530

    Feature test changes

--- src/main/java/com/releasenotes/ai_release_notes/AiReleaseNotesApplication.java
+++ src/main/java/com/releasenotes/ai_release_notes/AiReleaseNotesApplication.java
@@ -8,6 +8,8 @@ public class AiReleaseNotesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiReleaseNotesApplication.class, args);
+		System.out.println("Started, checking in feature branch");
+		System.out.println("Started, checking in feature branch going to commit");
 	}
 
 }
